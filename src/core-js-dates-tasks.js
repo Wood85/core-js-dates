23a0ed7c4180c96b71b9cf5a18282a160b825e86
Aleksandr Krivoshein <megalistnn@gmail.com>
2024-03-22 20:47:43 +0300

    feat: solve some tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return Date.parse(date);
 }
 
 /**
@@ -31,8 +31,8 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  return date.toTimeString().substring(0, 8);
 }
 
 /**
@@ -46,8 +46,26 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const index = new Date(date).getDay();
+  switch (index) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+    default:
+      return 'Sunday';
+  }
 }
 
 /**
@@ -61,8 +79,18 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const getDay = date.getDay();
+  const ms = 86400000;
+  let delta;
+  if (getDay < 5) {
+    delta = 5 - date.getDay();
+  }
+  if (getDay >= 5) {
+    delta = 12 - date.getDay();
+  }
+  const fullMs = Date.parse(date) + delta * ms;
+  return new Date(fullMs);
 }
 
 /**
@@ -76,8 +104,9 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const date = new Date(year, month, 0).getDate();
+  return date;
 }
 
 /**
@@ -91,8 +120,13 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const ms = 86400000;
+  const startMs = Date.parse(dateStart);
+  const endMs = Date.parse(dateEnd);
+  const delta = endMs - startMs;
+  const period = delta / ms;
+  return period + 1;
 }
 
 /**
@@ -112,8 +146,14 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dateMs = Date.parse(date);
+  const startDateMs = Date.parse(period.start);
+  const endDateMs = Date.parse(period.end);
+  if (dateMs >= startDateMs && dateMs <= endDateMs) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -127,8 +167,8 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Date(date).toLocaleString('en', { timeZone: 'UTC' });
 }
 
 /**
@@ -143,8 +183,15 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let count = 0;
+  for (let i = 1; i <= new Date(year, month, 0).getDate(); i += 1) {
+    const date = new Date(year, month - 1, i);
+    if (date.getDay() === 6 || date.getDay() === 0) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
